89c915cd3e10c851d64af94c88c7711ad77d9af0
GussyGee <arniegirvaan@gmail.com>
2022-05-18 11:22:10 +0200

    cleaned up comments

--- stream.py
+++ stream.py
@@ -1,13 +1,7 @@
 import config, json
 import websocket
 
-# wscat -c wss://stream.data.alpaca.markets/v2/iex
-# {"action": "auth", "key": "PKTS7T5474RJFXA5N68G", "secret": "5prqSY2CZRb1zUN4JF534i8LTFgx733uVOCtj5J8"}
-
-# # for minute bars
-# {"action": "subscribe", "bars": ["AMD"]}
-
-
+# send authentication data and subscribe to stream
 def on_open(ws):
     print("opened")
     auth_data = {
@@ -15,22 +9,20 @@ def on_open(ws):
         "key": config.API_KEY,
         "secret": config.SECRET_KEY
     }
-
     ws.send(json.dumps(auth_data))
-
-    listen_message = {"action": "subscribe", "bars": ["AMD"]}
-
+    listen_message = {"action": "subscribe", "quotes": ["PTON"]}
     ws.send(json.dumps(listen_message))
 
-
+# print confirmation to console when payload received
 def on_message(ws, message):
     print("received a message")
     print(message)
 
+# print confirmation that connection is closed to console
 def on_close(ws):
     print("closed connection")
 
+# define websocket and invoke functions
 socket = "wss://stream.data.alpaca.markets/v2/iex"
-
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
 ws.run_forever()
